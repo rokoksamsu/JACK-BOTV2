1a284ac324d441532ec3198396bbc87b01856a4d
rokoksamsu <83199451+rokoksamsu@users.noreply.github.com>
2022-10-22 20:19:19 +0700

    Add files via upload

---
+++ Hisoka-Morou-master/config.js
@@ -0,0 +1,52 @@
+/**
+   * Create By JAMAL.
+   * Contact Me on wa.me/6282280420522
+   * Follow https://github.com/DikaArdnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': 'Your Key',
+}
+
+// Other
+global.owner = ['6282280420522']
+global.premium = ['6288292024190']
+global.packname = 'JACK-BOT'
+global.author = 'WhatsApp Bot'
+global.sessionName = 'JACK'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'BENTAR NGAB...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 50
+}
+global.thumb = fs.readFileSync('./lib/hisoka.jpg')
+global.visoka = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
